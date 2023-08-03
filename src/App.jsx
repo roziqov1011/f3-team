a8711de3c4d0fe8f17e39035ffe54e590ee6ae23
roziqov1011 <roziqov1011@gmail.com>
2023-08-03 14:09:57 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Map from './components/Map/Map';
+import map from './img/Group (1).svg'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>World Map</h1>
+      <Map/>
+    </div>
+  );
+}
+
+export default App;
