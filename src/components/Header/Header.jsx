3db483010e9512eb1a30a8db005fd16dbc7b84a6
Begzodbekov <begzodxusanboyev2006@gmail.com>
2023-08-05 14:44:17 +0300

    add begzod

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import './Header.scss'
+import Logo from '../../img/Headerlogo.png'
+import { Link } from 'react-router-dom'
 function Header() {
+  const [navcolor, setNav] = useState(0)
+  console.log(navcolor);
   return (
-    <div>
-      <div className="container">
-      <h1>Header</h1>
-      </div>
+    <div className='Header'>
+        <div className="nav">
+          <div className="container">
+                <Link to={'/home'} className='nav-logo'><img src={Logo} alt="" /></Link>
+            <ul className='nav-list'>
+              <li className='nav-items'>
+                <Link onClick={()=> setNav(0)} className={ navcolor == 0? "navcolor" :"nav-links"} >All Flight</Link>
+              </li>
+              <li className='nav-items'>
+                <Link onClick={()=> setNav(1)} className={ navcolor == 1? "navcolor" :"nav-links"} >Schedule</Link>
+              </li>
+              <li className='nav-items'>
+                <Link onClick={()=> setNav(2)} className={ navcolor == 2? "navcolor" :"nav-links"} >Passengers</Link>
+              </li>
+              <li className='nav-items'>
+                <Link onClick={()=> setNav(3)} className={ navcolor == 3? "navcolor" :"nav-links"} >Your Orders</Link>
+              </li>
+              <li className='nav-items'>
+                <Link className='nav-links headerbutton' >Let’s Fly</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
     </div>
   )
 }
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import './Hero.scss'
+import Background from '../../img/hero.png'
+import Location from '../../img/order_location.png'
+import Costumer from '../../img/order_costumer.png'
+import Add from '../../img/order_date.png'
 function Hero() {
+  const [order, setOrder] = useState(0)
   return (
-    <div>
-      <h1>Hero</h1>
+    <div className='Hero'>
+      <div className="container">
+        <div className="heroitems">
+          <div className="hero-background">
+            <img src={Background} alt="" />
+          </div>
+          <h1 className='hero-title'>Travel around the world</h1>
+          <p>One of the advantages of being disorganized is that one is always having surprising discoveries</p>
+        </div>
+        <div className="hero_order">
+          <div className="order-style">
+            <h4 onClick={()=> setOrder(0)} className={order == 0? 'orderblue': 'style-title'}>Economy</h4>
+            <h4 onClick={()=> setOrder(1)} className={order == 1? 'orderblues': 'style-title'}>Busniness </h4>
+            <h4 onClick={()=> setOrder(2)} className={order == 2? 'orderbluess': 'style-title'}>First</h4>
+          </div>
+          <div className="order-information">
+            <div>
+              <img src={Location} alt="" />
+              <ul>
+                <li>Location</li>
+                <li>Where are you going?</li>
+              </ul>
+            </div>
+            <div>
+              <img src={Costumer} alt="" />
+              <ul>
+                <li>Travelers</li>
+                <li>Add guest</li>
+              </ul>
+            </div>
+            <div>
+              <img src={Add} alt="" />
+              <ul>
+                <li>Check-in</li>
+                <li>Add date</li>
+              </ul>
+            </div>
+            <div>
+              <img src={Add} alt="" />
+              <ul>
+                <li>Check-out</li>
+                <li>Add date</li>
+              </ul>
+            </div>
+          </div>
+          <button>Book Now</button>
+        </div>  
+      </div>
     </div>
   )
 }
