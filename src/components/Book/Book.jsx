37ef896a1a430a37e8bcb167adb423d02a21f339
mrhakimov7 <hakimovabdurazzoq2008@gmail.com>
2023-08-05 15:01:42 +0300

    add h

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import './book.scss';
 function Book() {
   return (
     <div>
       <h1>Book</h1>
+      <div className='book'>
+        
+      </div>
     </div>
   )
 }
