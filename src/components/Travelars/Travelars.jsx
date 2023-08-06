6b4ce4f971ed6f93cfa47fb68e43cd1b32b5343c
uzwhite444 <gocsgo398@gmail.com>
2023-08-06 12:48:24 +0500

    probel

--- src/components/Travelars/Travelars.jsx
+++ src/components/Travelars/Travelars.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
-
+import './Travelars.scss'
 function Travelars() {
   return (
-    <div>
-      <h1>Travelars</h1>
+    <div className='travelars'>
+      <div className="cards">
+        <div className="card1">
+          <img src="" alt="" />
+          <img src="" alt="" />
+          <h3>
+
+          </h3>
+          <p>
+
+          </p>
+        </div>
+        <div className="card2">
+          <img src="" alt="" />
+          <img src="" alt="" />
+          <h3>
+
+          </h3>
+          <p>
+
+          </p>
+        </div>
+        <div className="card3">
+          <img src="" alt="" />
+          <img src="" alt="" />
+          <h3>
+
+          </h3>
+          <p>
+
+          </p>
+        </div>
+        <div className="card4">
+          <img src="" alt="" />
+          <img src="" alt="" />
+          <h3>
+
+          </h3>
+          <p>
+
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
