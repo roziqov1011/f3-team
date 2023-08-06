c877e3697cf0cf6cbd02ff6675ea2c5167ef5a92
Begzodbekov <begzodxusanboyev2006@gmail.com>
2023-08-06 15:55:09 +0500

    review

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -15,13 +15,13 @@ function Header() {
                 <Link onClick={()=> setNav(0)} className={ navcolor == 0? "navcolor" :"nav-links"} >All Flight</Link>
               </li>
               <li className='nav-items'>
-                <Link onClick={()=> setNav(1)} className={ navcolor == 1? "navcolor" :"nav-links"} >Schedule</Link>
+                <Link onClick={()=> setNav(1)} className={ navcolor == 1? "navcolor2" :"nav-links"} >Schedule</Link>
               </li>
               <li className='nav-items'>
-                <Link onClick={()=> setNav(2)} className={ navcolor == 2? "navcolor" :"nav-links"} >Passengers</Link>
+                <Link onClick={()=> setNav(2)} className={ navcolor == 2? "navcolor3" :"nav-links"} >Passengers</Link>
               </li>
               <li className='nav-items'>
-                <Link onClick={()=> setNav(3)} className={ navcolor == 3? "navcolor" :"nav-links"} >Your Orders</Link>
+                <Link onClick={()=> setNav(3)} className={ navcolor == 3? "navcolor4" :"nav-links"} >Your Orders</Link>
               </li>
               <li className='nav-items'>
                 <Link className='nav-links headerbutton' >Let’s Fly</Link>
